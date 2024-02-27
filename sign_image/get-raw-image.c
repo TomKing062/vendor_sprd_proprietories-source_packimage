a9557bc50fd4d556a979fecc76f98ca314d06bf3
TomKing062 <yx42_44@126.com>
2024-02-27 11:33:13 +0800

    fix

--- sign_image/get-raw-image.c
+++ sign_image/get-raw-image.c
@@ -29,29 +29,21 @@ int main(int argc, char** argv)
     char* filename = argv[1];
     uint8_t* mem; size_t size = 0;
     mem = loadfile(filename, &size, 0);
+    uint8_t* mem0 = mem;
     if (!mem) ERR_EXIT("loadfile(\"%s\") failed\n", filename);
 
-    if (!(*(uint64_t*)&mem[0x30])) ERR_EXIT("The file is not sprd trusted firmware\n");
+    if (*(uint32_t*)mem != 0x42544844 || !(*(uint32_t*)&mem[0x30])) ERR_EXIT("The file is not sprd trusted firmware\n");
     size = *(uint64_t*)&mem[0x30];
     printf("0x%x\n", size);
 
-    int len = strlen(argv[1]);
-    char* newFilename = (char*)malloc((len - 5) * sizeof(char));
-    if (newFilename == NULL) {
-        printf("Error: Memory allocation failed.\n");
-        return 1;
-    }
-    strncpy(newFilename, filename, len - 9);  // 复制原文件名，去掉原来的后缀
-    strncpy(newFilename + len - 9, ".bin", 4);  // 加上新的后缀
-    newFilename[len -5] = '\0';  // 添加字符串结束符
     mem += 0x200;
-    FILE* file = fopen(newFilename, "wb");  // 以二进制模式打开文件
+    FILE* file = fopen(filename, "wb");
     if (file == NULL) ERR_EXIT("Failed to create the file.\n");
     size_t bytes_written = fwrite(mem, sizeof(unsigned char), size, file);
     if (bytes_written != size) ERR_EXIT("Failed to write the file.\n");
     fclose(file);
 
-    free(mem);
+    free(mem0);
 
     return 0;
 }
